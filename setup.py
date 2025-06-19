cd39f437663664f569513717062dbf2858d2633c
Swaraj Nayak <nswaraj02@gmail.com>
2025-06-19 16:42:09 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Swaraj Nayak ",
+    author_email="nswaraj02@gmail.com",
+    packages=find_packages()
+)
